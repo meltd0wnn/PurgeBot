3f439b384b3271f6c356f6eaa3af91816ea04037
Álvaro Faustino <56855863+meltd0wnn@users.noreply.github.com>
2020-07-27 16:30:49 -0300

    Loop working
    Just have to make purge work and it's ready to go

--- channelpurge.py
+++ channelpurge.py
@@ -7,46 +7,39 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Pronto para apagar segredos.')
 
+canal_de_mensagens = None
+
 @client.command()
 async def canal(ctx, target_channel_id):
-    if target_channel_id is None:
-        return ctx.send('Insira um canal!')
-    else:
-        canal_de_mensagens = client.get_channel(target_channel_id)
-        await ctx.send(f'Novo canal selecionado.\nCanal: #{canal_de_mensagens}\nChannel ID: {target_channel_id}')
-        print(f'Atualmente no canal #{canal_de_mensagens}')
-        return canal
+    global canal_de_mensagens
+    canal_de_mensagens = client.get_channel(int(target_channel_id))
+    await ctx.send(f'Novo canal selecionado.\nCanal: #{canal_de_mensagens}\nChannel ID: {target_channel_id}')
+    print(f'Atualmente no canal #{canal_de_mensagens}')
+    return int(target_channel_id)
 
-"""
-@tasks.loop(seconds= 10)
-async def called_once_a_day():
-    message_channel = client.get_channel(737011020446433404)
-    print(f"Got channel #{message_channel}")
-    await message_channel.send("Your message")
+@canal.error
+async def canal_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Insira um canal!')
 
-@called_once_a_day.before_loop
-async def before():
-    await client.wait_until_ready()
-    print("Finished waiting")
-
-called_once_a_day.start()
-
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=10)
 async def apagadiario():
-    while True:
-        #canalz = client.get_channel(canal)
-        #print(f'Atualmente no canal {canalz}')
-        await canal.channel.purge(limit=2)
-        await asyncio.sleep(1)
+    print(f"Loopando em #{canal_de_mensagens}")
+    if canal_de_mensagens is None:
+        pass
+    else:
+        print(f'Apagando mensagens em #{canal_de_mensagens}')
+        await canal_de_mensagens.channel.purge(2)
 
 @apagadiario.before_loop
 async def before():
     await client.wait_until_ready()
-    print('A espera acabou!')
+    print("Acabou a espera!")
 
 apagadiario.start()
 
-------------
+
+"""
 @client.command(aliases=['cc'])
 async def cleanchat(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
