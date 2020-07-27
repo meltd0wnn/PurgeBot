1a85e2add4ed7946ac671f19c0e2635c50c3d537
Álvaro Faustino <56855863+meltd0wnn@users.noreply.github.com>
2020-07-27 17:58:40 -0300

    Last update for start working
    Now the bot is purging amount messages per day and working in multiple channels just using the command (but i guess it only works in one server at a time)

--- channelpurge.py
+++ channelpurge.py
@@ -14,22 +14,24 @@ async def canal(ctx, target_channel_id):
     global canal_de_mensagens
     canal_de_mensagens = client.get_channel(int(target_channel_id))
     await ctx.send(f'Novo canal selecionado.\nCanal: #{canal_de_mensagens}\nChannel ID: {target_channel_id}')
-    print(f'Atualmente no canal #{canal_de_mensagens}')
-    return int(target_channel_id)
+    print(f'Entrando no canal #{canal_de_mensagens}')
+    return ctx
 
 @canal.error
 async def canal_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Insira um canal!')
 
-@tasks.loop(seconds=10)
+@tasks.loop(days=1)
 async def apagadiario():
-    print(f"Loopando em #{canal_de_mensagens}")
     if canal_de_mensagens is None:
+        print(f"Atuando em nenhum canal!")
         pass
     else:
-        print(f'Apagando mensagens em #{canal_de_mensagens}')
-        await canal_de_mensagens.channel.purge(2)
+        amount=10
+        print(f'Apagando mensagens em #{canal_de_mensagens}.\nTaxa: {amount} mensagens/dia')
+        await canal_de_mensagens.purge(limit=amount)
+        await canal_de_mensagens.send("MENSAGEM DO CANAL")
 
 @apagadiario.before_loop
 async def before():
@@ -37,14 +39,5 @@ async def before():
     print("Acabou a espera!")
 
 apagadiario.start()
-
-
-"""
-@client.command(aliases=['cc'])
-async def cleanchat(ctx, amount=10):
-    await ctx.channel.purge(limit=amount)
-    await ctx.send("MENSAGEM DE REGRA DO CANAL")
-
-"""
 
 client.run('TOKEN')
